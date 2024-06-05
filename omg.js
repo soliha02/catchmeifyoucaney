96d2a02e392f3b5c6b866ff1056e93101b01eb9b
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-05 10:49:06 +0500

    Add files via upload

---
+++ omg.js
@@ -0,0 +1,9 @@
+let ism = prompt('ism kiriting')
+let harf = prompt('harf kiriting')
+
+
+if(ism.includes(harf)){
+    alert(`${ism}   ismda ${harf} harfi bor`)
+}else{
+    alert(`${ism}   ismda ${harf} harfi yoq`)
+}
